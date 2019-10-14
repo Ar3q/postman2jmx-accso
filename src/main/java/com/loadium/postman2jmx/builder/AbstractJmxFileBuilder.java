ab618050a4ac53fbc79f1790fe8f49c03ce88682
Frederic Jacob <frederic.jacob@accso.de>
2019-10-14 13:49:43 +0200

    feat: preserve Postman order of requests in JMeter test plan

--- src/main/java/com/loadium/postman2jmx/builder/AbstractJmxFileBuilder.java
+++ src/main/java/com/loadium/postman2jmx/builder/AbstractJmxFileBuilder.java
@@ -14,6 +14,7 @@ import org.apache.jmeter.save.SaveService;
 import org.apache.jmeter.testelement.TestPlan;
 import org.apache.jmeter.threads.ThreadGroup;
 import org.apache.jorphan.collections.HashTree;
+import org.apache.jorphan.collections.ListedHashTree;
 
 import java.io.*;
 import java.util.ArrayList;
@@ -59,15 +60,15 @@ public abstract class   AbstractJmxFileBuilder implements IJmxFileBuilder {
         }
 
         // Create TestPlan hash tree
-        HashTree testPlanHashTree = new HashTree();
+        HashTree testPlanHashTree = new ListedHashTree();
         testPlanHashTree.add(testPlan);
 
         // Add ThreadGroup to TestPlan hash tree
-        HashTree threadGroupHashTree = new HashTree();
+        HashTree threadGroupHashTree = new ListedHashTree();
         threadGroupHashTree = testPlanHashTree.add(testPlan, threadGroup);
 
         // Add Http Sampler to ThreadGroup hash tree
-        HashTree httpSamplerHashTree = new HashTree();
+        HashTree httpSamplerHashTree = new ListedHashTree();
 
         // Add header manager hash tree
         HashTree headerHashTree = null;
